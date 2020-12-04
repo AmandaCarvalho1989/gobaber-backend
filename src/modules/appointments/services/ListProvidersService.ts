ee941ec78d1b81351d8e4785ab0e7940b25875d7
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2020-12-04 17:53:46 -0300

    Add classToClass

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 
 interface Request {
@@ -23,13 +24,13 @@ class ListProvidersService {
     public async execute({ user_id, }: Request): Promise<User[]> {
 
         let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`)
-
+        //let users;
         if (!users) {
             users = await this.usersRepository.findAllProviders({
                 except_user_id: user_id
             })
 
-            await this.cacheProvider.save(`providers-list:${user_id}`, users)
+            await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users))
         }
 
         return users
